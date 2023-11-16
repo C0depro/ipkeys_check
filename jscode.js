6f83599c799b2accca542810b4c8619fbef2ab0e
C0depro <150911292+C0depro@users.noreply.github.com>
2023-11-16 23:08:39 +0100

    Update jscode.js

--- jscode.js
+++ jscode.js
@@ -1,1 +1,17 @@
-dsqq
+const bot = new Bot("6652931725:AAHb5wB3amj5JfGYqDSHhRsPmj5D2D-qM8M", "5090243433");
+ 
+// bot.getUpdates()
+//   .then(res => {
+//     console.log(res.result);
+//   })
+document.getElementById("form")
+.addEventListener("submit", e => {
+  e.preventDefault();
+ 
+  const text = document.getElementById("textInput").value;
+ 
+  bot.sendFile("#fileInput", text)
+  .then(res => {
+    console.log(res);
+  })
+})
